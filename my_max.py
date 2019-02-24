d1112a0f50671081c928f380e700fb76be0db88c
Sonya44 <47757135+Sonya44@users.noreply.github.com>
2019-02-24 16:12:31 +0300

    Add files via upload

---
+++ my_max.py
@@ -0,0 +1,9 @@
+a=int(input('Введите первое число')
+b=int(input('Введите второе число')
+      if a>b:
+        print(a)
+      elif b>a:
+        print(b)
+      else :
+        print('Равны')
+           
