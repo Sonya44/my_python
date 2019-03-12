815356e2c26b808cff534b35eb9f3074f49637bf
Sonya44 <47757135+Sonya44@users.noreply.github.com>
2019-03-12 23:01:40 +0300

    Update my_max.py

--- my_max.py
+++ my_max.py
@@ -1,5 +1,5 @@
-a=int(input('Введите первое число')
-b=int(input('Введите второе число')
+a=int(input('Введите первое число'))
+b=int(input('Введите второе число'))
       if a>b:
         print(a)
       elif b>a:
